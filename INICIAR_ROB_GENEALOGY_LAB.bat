98dc411a26397cd7232ad22650f4336defed82dc
Sendin61 <151409919+Sendin61@users.noreply.github.com>
2026-08-14 16:02:29 +0200

    ux(lab): open local app explicitly in Chrome when available

--- INICIAR_ROB_GENEALOGY_LAB.bat
+++ INICIAR_ROB_GENEALOGY_LAB.bat
@@ -14,7 +14,16 @@ if not exist ".venv\Scripts\python.exe" (
 
 if not exist "%USERPROFILE%\Downloads\ROB-Genealogy-Lab" mkdir "%USERPROFILE%\Downloads\ROB-Genealogy-Lab"
 
-start "" powershell -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Milliseconds 1200; Start-Process 'http://127.0.0.1:8877'"
+set "ROB_CHROME="
+if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" set "ROB_CHROME=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
+if not defined ROB_CHROME if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" set "ROB_CHROME=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
+if not defined ROB_CHROME if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" set "ROB_CHROME=%LocalAppData%\Google\Chrome\Application\chrome.exe"
+
+if defined ROB_CHROME (
+  start "" powershell -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Milliseconds 1400; Start-Process '%ROB_CHROME%' 'http://127.0.0.1:8877'"
+) else (
+  start "" powershell -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Milliseconds 1400; Start-Process 'http://127.0.0.1:8877'"
+)
 
 echo [ROB] Iniciando ROB Genealogy Lab en http://127.0.0.1:8877
 ".venv\Scripts\python.exe" -m rob.lab.local_app
